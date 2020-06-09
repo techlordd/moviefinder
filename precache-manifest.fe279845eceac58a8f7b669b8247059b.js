00cc6ff3c9b34446abc90c8c599541ec3d3392b6
afrievents <afrieventsdiary@gmail.com>
2020-06-09 20:40:40 +0100

    Updates

---
+++ precache-manifest.fe279845eceac58a8f7b669b8247059b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc72aec4cde0069b3cadefd92720f12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e9dbc601242b7e089eb",
+    "url": "/static/css/2.b5bb3412.chunk.css"
+  },
+  {
+    "revision": "1685494932cfd668907d",
+    "url": "/static/css/main.f48531ed.chunk.css"
+  },
+  {
+    "revision": "3e9dbc601242b7e089eb",
+    "url": "/static/js/2.43b7002c.chunk.js"
+  },
+  {
+    "revision": "e23d03e7dd088bde7336b2c1d1349dae",
+    "url": "/static/js/2.43b7002c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1685494932cfd668907d",
+    "url": "/static/js/main.2bd65046.chunk.js"
+  },
+  {
+    "revision": "0dbcec08acf6024f155f",
+    "url": "/static/js/runtime-main.353f42f1.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/moviefinder/precache-manifest.e8b850072fa87cf478a07c084dcff6df.js"
+  "/precache-manifest.fe279845eceac58a8f7b669b8247059b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviefinder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
