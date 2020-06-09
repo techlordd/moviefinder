9a5e6cfad1332a65637095fba95f4fc654104654
afrievents <afrieventsdiary@gmail.com>
2020-06-09 20:13:11 +0100

    Updates

---
+++ precache-manifest.e8b850072fa87cf478a07c084dcff6df.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd9a7ed1b834369e16715f35e2a481d",
+    "url": "/moviefinder/index.html"
+  },
+  {
+    "revision": "02e4523d041aba7a70a0",
+    "url": "/moviefinder/static/css/2.28cce3e6.chunk.css"
+  },
+  {
+    "revision": "619d7111403769e8017c",
+    "url": "/moviefinder/static/css/main.f48531ed.chunk.css"
+  },
+  {
+    "revision": "02e4523d041aba7a70a0",
+    "url": "/moviefinder/static/js/2.10993d13.chunk.js"
+  },
+  {
+    "revision": "e23d03e7dd088bde7336b2c1d1349dae",
+    "url": "/moviefinder/static/js/2.10993d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "619d7111403769e8017c",
+    "url": "/moviefinder/static/js/main.893279c3.chunk.js"
+  },
+  {
+    "revision": "f549c356181eb6840722",
+    "url": "/moviefinder/static/js/runtime-main.2f020391.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/moviefinder/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/moviefinder/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/moviefinder/static/media/revicons.2feb69cc.eot"
+  }
+]);
